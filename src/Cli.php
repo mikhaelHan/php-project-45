52fca5cad8fac40f585637796604f7726fef2038
mikhaelHan <hancharuk270185@mail.ru>
2026-02-21 15:54:07 +0300

    feat: add task_3

---
+++ src/Cli.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BrainGames\Cli;
+
+use function \cli\line;
+use function \cli\prompt;
+
+class Cli
+{
+  public function greet()
+  {
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+  }
+}
